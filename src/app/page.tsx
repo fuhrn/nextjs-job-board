377c21e236567c62079b6c025c4b7a28ca7bd95f
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2024-04-14 21:48:15 -0300

    Chapter 5. Job List Server Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import Image from 'next/image'
+import JobListItem from "@/components/JobListItem";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    orderBy: {createdAt: "desc"}
+  })
   return (
-
-  )
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tighter lg:text-5xl">
+          Developer jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream job</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }

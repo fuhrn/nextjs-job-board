f176e00fec9710f74c08b8819ece2ce877e5d779
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2024-04-15 21:16:59 -0300

    Chapter 13. UseFormState

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { formatDistanceToNowStrict } from "date-fns"
+import { User } from "@clerk/nextjs/server";
+import { UserResource } from "@clerk/types";
+import { clsx, type ClassValue } from "clsx";
+import { formatDistanceToNowStrict } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function formatMoney(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-  }).format(amount)
+  }).format(amount);
 }
 
 export function relativeDate(from: Date) {
@@ -22,4 +24,8 @@ export function toSlug(str: string) {
     .toLowerCase()
     .replace(/ /g, "-")
     .replace(/[^\w-]+/g, "");
+}
+
+export function isAdmin(user: UserResource | User) {
+  return user.publicMetadata?.role === "admin";
 }
